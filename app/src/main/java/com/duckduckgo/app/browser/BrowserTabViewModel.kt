9d7f92a77911414ab0bb9cc9da8fc8b34c3cfd72
Anastasia Shuba <ashuba22@gmail.com>
2023-10-30 12:12:29 -0700

    address code review comments

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -461,10 +461,6 @@ class BrowserTabViewModel @Inject constructor(
             val request: PermissionRequest,
         ) : Command()
 
-        class DenySitePermissionRequest(
-            val request: PermissionRequest,
-        ) : Command()
-
         class ShowUserCredentialSavedOrUpdatedConfirmation(
             val credentials: LoginCredentials,
             val includeShortcutToViewCredential: Boolean,
@@ -1490,12 +1486,6 @@ class BrowserTabViewModel @Inject constructor(
                 command.postValue(ShowSitePermissionsDialog(sitePermissionsToAsk, request))
             }
         }
-    }
-
-    override fun onSitePermissionDenied(
-        request: PermissionRequest,
-    ) {
-        command.postValue(DenySitePermissionRequest(request))
     }
 
     override fun onSiteLocationPermissionRequested(
